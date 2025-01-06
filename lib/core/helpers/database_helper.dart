428286fccd38a278f4a392d5121aa9358ff2f9a5
12-jubal <12fbjp@gmail.com>
2025-01-06 02:50:02 +0100

    commenting

--- lib/core/helpers/database_helper.dart
+++ lib/core/helpers/database_helper.dart
@@ -3,18 +3,22 @@ import 'dart:developer';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+/// A helper class to manage the SQLite database for journal entries.
 class DatabaseHelper {
+  // Singleton instance of DatabaseHelper
   // DatabaseHelper._();
   // static final DatabaseHelper db = DatabaseHelper._();
 
   static Database? _database;
 
+  /// Getter for the database instance. Initializes the database if it doesn't exist.
   Future<Database> get database async {
     if (_database != null) return _database!;
     _database = await _initDatabase();
     return _database!;
   }
 
+  /// Initializes the database and creates the 'journals' table if it doesn't exist.
   Future<Database> _initDatabase() async {
     final dbPath = await getDatabasesPath();
     final path = join(dbPath, 'journal.db');
@@ -35,10 +39,19 @@ class DatabaseHelper {
     );
   }
 
-  Future<int> addJournal(
-      {required String mood,
-      required String content,
-      required String date}) async {
+  /// Adds a new journal entry to the 'journals' table.
+  ///
+  /// Parameters:
+  /// - `mood`: The mood of the journal entry.
+  /// - `content`: The content of the journal entry.
+  /// - `date`: The date of the journal entry.
+  ///
+  /// Returns the id of the inserted journal entry.
+  Future<int> addJournal({
+    required String mood,
+    required String content,
+    required String date,
+  }) async {
     final db = await database;
     log('Adding journal entry', name: 'DatabaseHelper');
     return await db.insert('journals', {
@@ -48,6 +61,9 @@ class DatabaseHelper {
     });
   }
 
+  /// Fetches all journal entries from the 'journals' table.
+  ///
+  /// Returns a list of maps containing the journal entries.
   Future<List<Map<String, dynamic>>> fetchJournals() async {
     final db = await database;
     final result = await db.query('journals');
@@ -62,6 +78,12 @@ class DatabaseHelper {
     }).toList();
   }
 
+  /// Deletes a journal entry from the 'journals' table by id.
+  ///
+  /// Parameters:
+  /// - `id`: The id of the journal entry to delete.
+  ///
+  /// Returns the number of rows affected.
   Future<int> deleteJournal({required int id}) async {
     final db = await database;
     return await db.delete('journals', where: 'id = ?', whereArgs: [id]);
